28f853d0a951c41927bd32167940e329fc406481
Jan Henning Thorsen <jhthorsen@cpan.org>
2012-11-11 01:41:06 +0000

    Import of JHTHORSEN/Mojo-TFTPd-0.01 from CPAN.
    gitpan-cpan-distribution: Mojo-TFTPd
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         JHTHORSEN/Mojo-TFTPd-0.01.tar.gz
    gitpan-cpan-author:       JHTHORSEN
    gitpan-cpan-maturity:     released

---
+++ lib/Mojo/TFTPd.pm
@@ -0,0 +1,317 @@
+package Mojo::TFTPd;
+
+=head1 NAME
+
+Mojo::TFTPd - Trivial File Transfer Protocol daemon
+
+=head1 VERSION
+
+0.01
+
+=head1 SYNOPSIS
+
+    use Mojo::TFTPd;
+    my $tftpd = Mojo::TFTPd->new;
+
+    $tftpd->on(error => sub {
+        warn "TFTPd: $_[1]\n";
+    });
+
+    $tftpd->on(rrq => sub {
+        my($tftpd, $c) = @_;
+        open my $FH, '<', $c->file;
+        $c->filehandle($FH);
+    });
+
+    $tftpd->on(wrq => sub {
+        my($tftpd, $c) = @_;
+        open my $FH, '>', '/dev/null';
+        $c->filehandle($FH);
+    });
+
+    $self->on(finish => sub {
+        my($tftpd, $c, $error) = @_;
+        warn "Connection: $error\n" if $error;
+    });
+
+    $tftpd->start;
+    $tftpd->ioloop->start unless $tftpd->ioloop->is_running;
+
+=head1 DESCRIPTION
+
+This module implement a server for the
+L<Trivial File Transfer Protocol|http://en.wikipedia.org/wiki/Trivial_File_Transfer_Protocol>.
+
+From Wikipedia:
+
+    Trivial File Transfer Protocol (TFTP) is a file transfer protocol notable
+    for its simplicity. It is generally used for automated transfer of
+    configuration or boot files between machines in a local environment.
+
+The connection ($c) which is referred to in this document is an instance of
+L<Mojo::TFTPd::Connection>.
+
+=cut
+
+use Mojo::Base 'Mojo::EventEmitter';
+use Mojo::IOLoop;
+use Mojo::TFTPd::Connection;
+use Mojo::URL;
+use constant OPCODE_RRQ => 1;
+use constant OPCODE_WRQ => 2;
+use constant OPCODE_DATA => 3;
+use constant OPCODE_ACK => 4;
+use constant OPCODE_ERROR => 5;
+use constant OPCODE_OACK => 6;
+use constant CHECK_INACTIVE_INTERVAL => $ENV{MOJO_TFTPD_CHECK_INACTIVE_INTERVAL} || 3;
+use constant DATAGRAM_LENGTH => $ENV{MOJO_TFTPD_DATAGRAM_LENGTH} || 512;
+use constant DEBUG => $ENV{MOJO_TFTPD_DEBUG} ? 1 : 0;
+
+our $VERSION = eval '0.01';
+
+=head1 EVENTS
+
+=head2 error
+
+    $self->on(error => sub {
+        my($self, $str) = @_;
+    });
+
+This event is emitted when something goes wrong: Fail to L</listen> to socket,
+read from socket or other internal errors.
+
+=head2 finish
+
+    $self->on(finish => sub {
+        my($self, $c, $error) = @_;
+    });
+
+This event is emitted when the client finish, either successfully or due to an
+error. C<$error> will be an empty string on success.
+
+=head2 rrq
+
+    $self->on(rrq => sub {
+        my($self, $c) = @_;
+    });
+
+This event is emitted when a new read request arrives from a client. The
+callback should set L<Mojo::TFTPd::Connection/filehandle> or the connection
+will be dropped.
+
+=head2 wrq
+
+    $self->on(wrq => sub {
+        my($self, $c) = @_;
+    });
+
+This event is emitted when a new write request arrives from a client. The
+callback should set L<Mojo::TFTPd::Connection/filehandle> or the connection
+will be dropped.
+
+=head1 ATTRIBUTES
+
+=head2 ioloop
+
+Holds an instance of L<Mojo::IOLoop>.
+
+=cut
+
+has ioloop => sub { Mojo::IOLoop->singleton };
+
+=head2 listen
+
+    $str = $self->server;
+    $self->server("127.0.0.1:69");
+    $self->server("tftp://*:69"); # any interface
+
+The bind address for this server.
+
+=cut
+
+has listen => 'tftp://*:69';
+
+=head2 max_connections
+
+How many concurrent connections this server can handle. Default to 1000.
+
+=cut
+
+has max_connections => 1000;
+
+=head2 retries
+
+How many times the server should try to send ACK or DATA to the client before
+dropping the L<connection|Mojo::TFTPd::Connection>.
+
+=cut
+
+has retries => 1;
+
+=head2 inactive_timeout
+
+How long a L<connection|Mojo::TFTPd::Connection> can stay idle before
+
+=cut
+
+has inactive_timeout => 15;
+
+=head1 METHODS
+
+=head2 start
+
+Starts listening to the address and port set in L</Listen>. The L</error>
+event wille be emitted if the server fail to start.
+
+=cut
+
+sub start {
+    my $self = shift;
+    my $reactor = $self->ioloop->reactor;
+    my $url = $self->listen;
+    my $socket;
+
+    $self->{connections} and return $self;
+    $self->{connections} = {};
+
+    if($url =~ /^tftp/) {
+        $url = Mojo::URL->new($url);
+    }
+    else {
+        $url = Mojo::URL->new("tftp://$url");
+    }
+
+    warn "[Mojo::TFTPd] Listen to $url\n" if DEBUG;
+
+    $socket = IO::Socket::INET->new(
+                  LocalAddr => $url->host eq '*' ? '0.0.0.0' : $url->host,
+                  LocalPort => $url->port,
+                  Proto => 'udp',
+              );
+
+    if(!$socket) {
+        delete $self->{connections};
+        return $self->emit(error => "Can't create listen socket: $!");
+    };
+
+    Scalar::Util::weaken($self);
+
+    $socket->blocking(0);
+    $reactor->io($socket, sub { $self->_incoming });
+    $reactor->watch($socket, 1, 0); # watch read events
+    $self->{socket} = $socket;
+    $self->{checker}
+        = $self->ioloop->recurring(CHECK_INACTIVE_INTERVAL || 3, sub {
+            my $timeout = time - $self->inactive_timeout;
+            for my $c (values %{ $self->{connections} }) {
+                $timeout < $c->{timestamp} and next;
+                $c->error('Inactive timeout');
+                $self->_delete_connection($c);
+            }
+        });
+
+    return $self;
+}
+
+sub _incoming {
+    my $self = shift;
+    my $socket = $self->{socket};
+    my $read = $socket->recv(my $datagram, DATAGRAM_LENGTH);
+    my($opcode, $connection);
+
+    if(!defined $read) {
+        return $self->emit(error => "Read: $!");
+    }
+
+    $opcode = unpack 'n', substr $datagram, 0, 2, '';
+
+    # new connection
+    if($opcode eq OPCODE_RRQ) {
+        return $self->_new_request(rrq => $datagram);
+    }
+    elsif($opcode eq OPCODE_WRQ) {
+        return $self->_new_request(wrq => $datagram);
+    }
+
+    # existing connection
+    $connection = $self->{connections}{$socket->peername};
+
+    if(!$connection) {
+        return $self->emit(error => "@{[$socket->peerhost]} has no connection");
+    }
+    elsif($opcode == OPCODE_ACK) {
+        return if $connection->receive_ack($datagram) and $connection->send_data;
+    }
+    elsif($opcode == OPCODE_DATA) {
+        return if $connection->receive_data($datagram) and $connection->send_ack;
+    }
+    elsif($opcode == OPCODE_ERROR) {
+        my($code, $msg) = unpack 'nZ*', $datagram;
+        $connection->error("($code) $msg");
+    }
+    else {
+        $connection->error("Unknown opcode");
+    }
+
+    # if something goes wrong or finish with connection
+    $self->_delete_connection($connection);
+}
+
+sub _new_request {
+    my($self, $type, $datagram) = @_;
+    my($file, $mode, @rfc) = split "\0", $datagram;
+    my $socket = $self->{socket};
+    my $connection;
+
+    warn "[Mojo::TFTPd] <<< @{[$socket->peerhost]} $type $file $mode @rfc\n" if DEBUG;
+
+    if(!$self->has_subscribers($type)) {
+        $self->emit(error => "Cannot handle $type requests");
+        return;
+    }
+    if($self->max_connections <= keys %{ $self->{connections} }) {
+        $self->emit(error => "Max connections ($self->{max_connections}) reached");
+        return;
+    }
+
+    $connection = Mojo::TFTPd::Connection->new(
+                        file => $file,
+                        mode => $mode,
+                        peerhost => $socket->peerhost,
+                        peername => $socket->peername,
+                        retries => $self->retries,
+                        rfc => \@rfc,
+                        socket => $socket,
+                    );
+
+    $self->emit($type => $connection);
+
+    if($type eq 'rrq' ? $connection->send_data : $connection->send_ack) {
+        $self->{connections}{$connection->peername} = $connection;
+    }
+    else {
+        $self->emit(finish => $connection, $connection->error);
+    }
+}
+
+sub _delete_connection {
+    my($self, $connection) = @_;
+    delete $self->{connections}{$connection->peername};
+    $self->emit(finish => $connection, $connection->error);
+}
+
+sub DEMOLISH {
+    my $self = shift;
+    my $reactor = eval { $self->ioloop->reactor } or return; # may be undef during global destruction
+
+    $reactor->remove($self->{checker}) if $self->{checker};
+    $reactor->remove($self->{socket}) if $self->{socket};
+}
+
+=head1 AUTHOR
+
+Jan Henning Thorsen - C<jhthorsen@cpan.org>
+
+=cut
+
+1;
---
+++ lib/Mojo/TFTPd/Connection.pm
@@ -0,0 +1,235 @@
+package Mojo::TFTPd::Connection;
+
+=head1 NAME
+
+Mojo::TFTPd::Connection - A connection class for Mojo::TFTPd
+
+=head1 SYNOPSIS
+
+See L<Mojo::TFTPd>
+
+=cut
+
+use Mojo::Base -base;
+use Socket;
+use constant OPCODE_DATA => 3;
+use constant OPCODE_ACK => 4;
+use constant OPCODE_ERROR => 5;
+use constant DATA_LENGTH => ($ENV{MOJO_TFTPD_DATAGRAM_LENGTH} || 512) - 4;
+use constant DEBUG => $ENV{MOJO_TFTPD_DEBUG} ? 1 : 0;
+
+our %ERROR_CODES = (
+    not_defined => [0, 'Not defined, see error message'],
+    unknown_opcode => [0, 'Unknown opcode: %s'],
+    no_connection => [0, 'No connection'],
+    file_not_found => [1, 'File not found'],
+    access_violation => [2, 'Access violation'],
+    disk_full => [3, 'Disk full or allocation exceeded'],
+    illegal_operation => [4, 'Illegal TFTP operation'],
+    unknown_transfer_id => [5, 'Unknown transfer ID'],
+    file_exists => [6, 'File already exists'],
+    no_such_user => [7, 'No such user'],
+);
+
+=head1 ATTRIBUTES
+
+=head2 error
+
+Useful to check inside L<Mojo::TFTPd/finish> events to see if anything has
+gone wrong. Holds a string describing the error.
+
+=head2 file
+
+The filename the client requested to read or write.
+
+=head2 filehandle
+
+This must be set inside the L<rrq|Mojo::TFTPd/rrq> or L<rrw|Mojo::TFTPd/rrw>
+event or the connection will be dropped.
+
+=head2 mode
+
+Either "ascii", "octet" or empty string if unknown.
+
+=head2 peerhost
+
+The IP address of the remove client.
+
+=head2 peername
+
+Packet address of the remote client.
+
+=head2 retries
+
+Number of times L</send_data> or L</send_ack> can be retried before the
+connection is dropped. This value comes from L<Mojo::TFTPd/retries>.
+
+=head2 socket
+
+The UDP handle to send data to.
+
+=head2 rfc
+
+Contains extra parameters the client has provided. These parameters are stored
+in an array ref.
+
+=cut
+
+has error => '';
+has file => '/dev/null';
+has filehandle => undef;
+has mode => '';
+has peerhost => '';
+has peername => '';
+has retries => 2;
+has rfc => sub { [] };
+has socket => undef;
+has _sequence_number => 1;
+
+=head1 METHODS
+
+=head2 send_data
+
+This method is called when the server sends DATA to the client.
+
+=cut
+
+sub send_data {
+    my $self = shift;
+    my $FH = $self->filehandle;
+    my $n = $self->_sequence_number;
+    my($data, $sent);
+
+    $self->{timestamp} = time;
+
+    if(!$FH) {
+        return $self->send_error(file_not_found => 'No filehandle');
+    }
+    elsif(not seek $FH, ($n - 1) * DATA_LENGTH, 0) {
+        return $self->send_error(file_not_found => "Seek: $!");
+    }
+    if(not defined read $FH, $data, DATA_LENGTH) {
+        return $self->send_error(file_not_found => "Read: $!");
+    }
+    if(length $data < DATA_LENGTH) {
+        $self->{_last_sequence_number} = $n;
+    }
+
+    warn "[Mojo::TFTPd] >>> $self->{peerhost} data $n (@{[length $data]})\n" if DEBUG;
+
+    $sent = $self->socket->send(
+                pack('nna*', OPCODE_DATA, $n, $data),
+                MSG_DONTWAIT,
+                $self->peername,
+            );
+
+    return 0 unless length $data;
+    return 1 if $sent;
+    $self->error("Send: $!");
+    return $self->{retries}--;
+}
+
+=head2 receive_ack
+
+This method is called when the client sends ACK to the server.
+
+=cut
+
+sub receive_ack {
+    my $self = shift;
+    my($n) = unpack 'n', shift;
+
+    warn "[Mojo::TFTPd] <<< $self->{peerhost} ack $n\n" if DEBUG;
+
+    return 0 if $self->{_last_sequence_number} and $n == $self->{_last_sequence_number};
+    return ++$self->{_sequence_number} if $n == $self->{_sequence_number};
+    $self->error('Invalid packet number');
+    return $self->{retries}--;
+}
+
+=head2 receive_data
+
+This method is called when the client sends DATA to the server.
+
+=cut
+
+sub receive_data {
+    my $self = shift;
+    my($n, $data) = unpack 'na*', shift;
+    my $FH = $self->filehandle;
+
+    warn "[Mojo::TFTPd] <<< $self->{peerhost} data $n (@{[length $data]})\n" if DEBUG;
+
+    unless($FH) {
+        return $self->send_error(illegal_operation => 'No filehandle');
+    }
+    unless($n == $self->_sequence_number) {
+        $self->error('Invalid packet number');
+        return $self->{retries}--;
+    }
+    unless(print $FH $data) {
+        return $self->send_error(illegal_operation => "Write: $!");
+    };
+    unless(length $data == DATA_LENGTH) {
+        $self->{_last_sequence_number} = $n;
+    }
+
+    $self->{_sequence_number}++;
+    return 1;
+}
+
+=head2 send_ack
+
+This method is called when the server sends ACK to the client.
+
+=cut
+
+sub send_ack {
+    my $self = shift;
+    my $n = $self->_sequence_number - 1;
+    my $sent;
+
+    $self->{timestamp} = time;
+    warn "[Mojo::TFTPd] >>> $self->{peerhost} ack $n\n" if DEBUG;
+
+    $sent = $self->socket->send(
+                pack('nn', OPCODE_ACK, $n),
+                MSG_DONTWAIT,
+                $self->peername,
+            );
+
+    return 0 if defined $self->{_last_sequence_number};
+    return 1 if $sent;
+    $self->error("Send: $!");
+    return $self->{retries}--;
+}
+
+=head2 send_error
+
+Used to report error to the client.
+
+=cut
+
+sub send_error {
+    my($self, $name) = @_;
+    my $err = $ERROR_CODES{$name} || $ERROR_CODES{not_defined};
+
+    warn "[Mojo::TFTPd] >>> $self->{peerhost} error @$err\n" if DEBUG;
+
+    $self->error($_[2]);
+    $self->socket->send(
+        pack('nnZ*', OPCODE_ERROR, @$err),
+        MSG_DONTWAIT,
+        $self->peername,
+    );
+
+    return 0;
+}
+
+=head1 AUTHOR
+
+Jan Henning Thorsen - C<jhthorsen@cpan.org>
+
+=cut
+
+1;
